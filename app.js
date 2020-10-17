70f0ae3603e2b1f2b0500c81dd2362b41f1fff89
Aditya Jee <jeeadityajha1@gmail.com>
2020-10-18 00:46:30 +0530

    Added Speakers/FAQs in Home, styled and edited Contact Form

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const app = express()
 
+const speakers = require('./speakers');
+//console.log(speakers)
+
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
@@ -9,36 +12,12 @@ app.use(express.json())
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
+
+
 app.get("/", function(req,res){
-    res.render("home")
+    res.render("home",{speakers : speakers})
 })
 
-app.get("/speakers", (req, res)=>{
-
-    const speakers = [
-        {
-            "name" : "Mark Zuckerbug",
-            "desc" : "Hello There",
-            "img_url" : "https://techtalklive.org/wp-content/uploads/2017/01/iStock-502642537-1.jpg"
-        },
-        {
-            "name" : "Mark Zuckerbug",
-            "desc" : "Hello There",
-            "img_url" : "https://techtalklive.org/wp-content/uploads/2017/01/iStock-502642537-1.jpg"
-        },{
-            "name" : "Mark Zuckerbug",
-            "desc" : "Hello There",
-            "img_url" : "https://techtalklive.org/wp-content/uploads/2017/01/iStock-502642537-1.jpg"
-        },{
-            "name" : "Mark Zuckerbug",
-            "desc" : "Hello There",
-            "img_url" : "https://techtalklive.org/wp-content/uploads/2017/01/iStock-502642537-1.jpg"
-        },
-    ]
-
-    res.render("speakers", {speakers : speakers} )
-
-})
 
 app.listen(3000, function(){
     console.log("Starting Server at Port 3000")
